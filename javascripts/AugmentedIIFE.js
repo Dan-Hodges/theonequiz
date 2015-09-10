6139aad177dd048638ed0283b1f8baa80e7a4388
Dan-Hodges <dhdgs690@gmail.com>
2015-09-10 13:29:11 -0500

    ud

--- javascripts/AugmentedIIFE.js
+++ javascripts/AugmentedIIFE.js
@@ -1,10 +1,10 @@
-var augmented = (function(FruitMaker) {
-  FruitMaker.setFruit = function(fruit, slices) { //cannot make Fruitmaker be defined here
-  	var chosenFruit = [];
-  	for (var i = 0; i < slices; i++) {
-  		chosenFruit.push(fruit);
-  	}
-  return chosenFruit;
-  }
-  return FruitMaker;
+var FruitMaker = (function(SliceMaker) {
+  SliceMaker.slices = function(fruit, slices) {
+    var slicedFruit = [];
+    for (var i = 0; i < slices; i++) {
+      slicedFruit.push(fruit);
+    }
+    return slicedFruit;
+  };
+  return SliceMaker;
 })(FruitMaker);
